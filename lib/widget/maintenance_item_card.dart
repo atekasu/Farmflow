74b0e43254b4adb7d79a7aea214d6972513e3a1c
atekasu <ataranaize@gmail.com>
2025-08-17 20:36:24 +0900

    リスト化を終了　ディテールは明日

--- lib/widget/maintenance_item_card.dart
+++ lib/widget/maintenance_item_card.dart
@@ -3,19 +3,18 @@ import 'package:flutter/material.dart';
 import '../model/machine.dart';
 
 class maintenanceItemCard extends StatelessWidget {
-  final List<Machine> machine;
-  const maintenanceItemCard({
-    Key? key,
-    required this.machine,
-  }) : super(key: key);
-
-  List<Widget> get _maintenanceItemList{
-    return machine.component.map((component){
-
-    })
-  }
+  final Machine machine;
+  const maintenanceItemCard({Key? key, required this.machine})
+    : super(key: key);
   @override
-  Widget build(BuildContext context){
-
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: machine.components.length,
+      itemBuilder: (context, index) {
+        final item = machine.components[index];
+        return Card(child: ListTile(title: Text(item.name)));
+      },
+    );
   }
 }
