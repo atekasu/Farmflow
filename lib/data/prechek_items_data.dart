2730d0278d6150ccdaa2c840e4a371f48e82b9a5
atekasu <ataranaize@gmail.com>
2025-09-15 11:10:13 +0900

    点検リスト追加

---
+++ lib/data/prechek_items_data.dart
@@ -0,0 +1,57 @@
+import 'package:farmflow/model/precheck_item.dart';
+
+class PrechekItemsData {
+  static const List<PreCheckItem> items = [
+    //トラクター本体
+    PreCheckItem(
+      id: 'engin_oil_check',
+      name: 'エンジンオイル',
+      description: 'レベルゲージの確認',
+      category: PreCheckCategory.tractor,
+    ),
+    PreCheckItem(
+      id: 'coolant_check',
+      name: 'クーラント',
+      description: 'リザーバータンクの確認',
+      category: PreCheckCategory.tractor,
+    ),
+    PreCheckItem(
+      id: 'grease_check',
+      name: 'グリス',
+      description: 'グリスポイントの確認',
+      category: PreCheckCategory.tractor,
+    ),
+    PreCheckItem(
+      id: 'air_filter_check',
+      name: 'エアフィルター',
+      description: 'エレメントの汚れの確認、交換の有無',
+      category: PreCheckCategory.tractor,
+    ),
+    PreCheckItem(
+      id: 'tire',
+      name: 'タイヤ',
+      description: '空気圧と摩耗具合の確認',
+      category: PreCheckCategory.tractor,
+    ),
+
+    //ロータリー
+    PreCheckItem(
+      id: 'chain_cover',
+      name: 'チェーンカバー',
+      description: '摩耗具合の点検',
+      category: PreCheckCategory.rotary,
+    ),
+    PreCheckItem(
+      id: 'gir_oil',
+      name: 'ギアオイル',
+      description: '残量点検 ',
+      category: PreCheckCategory.rotary,
+    ),
+    PreCheckItem(
+      id: 'tilling_claw',
+      name: '耕うん爪',
+      description: '緩み、摩耗具合の点検',
+      category: PreCheckCategory.rotary,
+    ),
+  ];
+}
