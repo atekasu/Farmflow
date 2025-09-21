1aa044090e5e2b777edfb4ca1d205999b6312e48
atekasu <ataranaize@gmail.com>
2025-09-21 18:50:04 +0900

    始業前点検画面を追加

--- lib/model/precheck_item.dart
+++ lib/model/precheck_item.dart
@@ -10,6 +10,30 @@ class PreCheckItem {
   final String name;
   final String description;
   final PreCheckCategory category;
+
+  PreCheckItem copyWith({
+    String? id,
+    String? name,
+    String? description,
+    PreCheckCategory? category,
+  }) {
+    return PreCheckItem(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      category: category ?? this.category,
+    );
+  }
 }
 
-enum PreCheckCategory { tractor, rotary }
+enum PreCheckCategory { 
+  tractor, //トラクター
+  rotary,  //ロータリー
+  }
+
+enum CheckStatus {
+  notChecked, //未選択
+  good,       //問題なし
+  warning,    //注意 
+  critical,   //破損、交換
+  }
